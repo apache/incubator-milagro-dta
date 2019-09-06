aa7ab6f8a4ecbd5c24db443acaa327fa9565b718
Stanislav Mihaylov <smihaylov@users.noreply.github.com>
2019-09-06 12:16:00 +0300

    Add Travis (#31)
    * Add travis
    
    * Fix formatting

--- lint.sh
+++ lint.sh
@@ -4,4 +4,4 @@ echo "Run go fmt"
 test -z "$(gofmt -s -l . 2>&1 | grep -v vendor | tee /dev/stderr)"
 
 echo "Run go lint"
-golint -set_exit_status $(go list ./... | grep -v /vendor/)
+golint -set_exit_status $(go list ./...)
